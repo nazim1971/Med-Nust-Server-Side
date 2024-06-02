4e05f08ddc2455c20935816ad2937f7498ef2d6f
nazim1971 <md.nazimuddinaj@gmail.com>
2024-06-02 11:56:48 +0600

    User Registration and Authentication complete

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
 // middleware
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 
 
@@ -30,6 +36,29 @@ async function run() {
     // collections
     const categoryCollection = client.db("medDB").collection('category');
     const categoryNameCollection = client.db("medDB").collection('categoryName');
+    const usersCollection = client.db("medDB").collection('users');
+
+    // jwt relrated api
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'});
+      res.send({token});
+    })
+
+    // middlewares
+    const verifyToken = (req,res,next) =>{
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'unauthorized access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
 
     // all medicine
     app.get('/category', async(req,res)=>{
@@ -43,7 +72,22 @@ async function run() {
         const result = await categoryNameCollection.find().toArray();
         res.send(result)
     })
+      // users releted api
+      app.post('/users',async(req,res)=>{
+        const user = req.body;
+        const query = {email: user.email};
+        const existingUser = await usersCollection.findOne(query);
+        if(existingUser){
+          return res.send({message: 'user already exists', insertedId: null})
+        }
+        const result = await  usersCollection.insertOne(user);
+        res.send(result);
+      })
 
+      app.get('/users',verifyToken, async (req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      });
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
